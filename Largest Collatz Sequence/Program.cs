9f14ed5e03549be54131a7350e688de9af3fadf6
danikay tech <danikay tech@LAPTOP-3J25IRPG>
2021-09-01 23:43:01 +0100

    Add project files.

---
+++ Largest Collatz Sequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Largest_Collatz_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong longestChain = 13;
+            for(ulong i = 13; i < 1000000; i++)
+            {
+                if(CollatzSequence(longestChain) < CollatzSequence(i))
+                {
+                    longestChain = i;
+                }
+            }
+            Console.WriteLine(longestChain);
+            Console.WriteLine("With " + CollatzSequence(longestChain) + " terms");
+            // Is to return the number of terms of collatz sequence a number contains
+            static ulong CollatzSequence(ulong n)
+            {
+                ulong evenCount = 0;
+                ulong oddCount = 0;
+                while (n != 1)
+                {
+                    bool passed = true;
+                    if (n % 2 == 0)//is even
+                    {
+                        n /= 2;
+                        evenCount++;
+                        if (n % 2 != 0) //did this so after dividing the even and an odd number is got
+                        {              // it won't return to the odd below else goes to the return value.
+                            passed = false;
+                        }
+                    }
+                    if (n % 2 != 0 && n != 1 && passed == true)//is odd
+                    {
+                        n = (3 * n) + 1;
+                        oddCount++;
+                    }
+                }
+                return (oddCount + evenCount + 1);//return the no of terms
+            }
+            Console.ReadLine();
+        }
+    }
+}
